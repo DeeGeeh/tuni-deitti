32f7c01e4332832685d53e7cb2c456a5bc48efd8
Diar Ghaderi <diar.ghaderi@gmail.com>
2025-02-28 23:42:26 +0200

    Add: link to homepage

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { usePathname } from "next/navigation";
 import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 export default function Navbar() {
   const pathname = usePathname(); // Get current path
@@ -10,8 +11,9 @@ export default function Navbar() {
     <nav className="bg-white/80 backdrop-blur-sm fixed w-full z-10">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4">
         <div className="flex justify-between items-center">
-          <h1 className="text-2xl font-bold text-tuni-blue">TuniDeitti</h1>
-
+          <Link href="/" className="text-2xl font-bold text-tuni-blue">
+            TuniDeitti
+          </Link>
           {/* Show Log in button only on the landing page */}
           {pathname === "/" && (
             <button
