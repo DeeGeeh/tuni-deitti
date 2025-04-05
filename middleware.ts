1d45be1ba5f0c024a0a34434847323a706a767c6
Diar Ghaderi <diar.ghaderi@gmail.com>
2025-04-06 01:55:21 +0300

    maintanance mode again :DD

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(req: NextRequest) {
-  const maintenanceMode = false; // Set to true when the site is under maintenance
+  const maintenanceMode = true; // Set to true when the site is under maintenance
 
   if (maintenanceMode && !req.nextUrl.pathname.startsWith("/maintenance")) {
     return NextResponse.redirect(new URL("/maintenance", req.url));
