640a57cd1d5e51694cfcc25dc66c239d732d1f31
Diar Ghaderi <diar.ghaderi@gmail.com>
2025-04-05 22:15:50 +0300

    bandaid fix for scaling

--- app/(app)/swipe/page.tsx
+++ app/(app)/swipe/page.tsx
@@ -1,10 +1,13 @@
 // MAIN SWIPING APP
 import SwipeableCard from "@/app/components/Profilecard";
-import { Auth } from "firebase/auth";
 
 export default function SwipePage() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+    <div
+      className="flex flex-col items-center justify-center bg-gray-100"
+      style={{ height: "calc(100vh - 124px)" }}
+    >
+      {/* Subtract the footer height (64px) */}
       <SwipeableCard />
     </div>
   );
