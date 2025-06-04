ebadb6cb1438c95daf16660735ce8cdeebee99fd
Diar Ghaderi <diar.ghaderi@gmail.com>
2025-06-05 00:01:03 +0300

    remove redundant code

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,21 +1,10 @@
 // app/(app)/layout.tsx
-"use client";
-
-import React, { useState, useEffect } from "react";
-import { usePathname } from "next/navigation";
+import React from "react";
 import AppNavbar from "../components/AppNavbar";
 import AppFooter from "../components/AppFooter";
 import ProtectedRoute from "../components/ProtectedRoute";
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
-  const pathname = usePathname();
-  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
-
-  // Close mobile menu when route changes
-  useEffect(() => {
-    setIsMobileMenuOpen(false);
-  }, [pathname]);
-
   return (
     <ProtectedRoute>
       <div className="min-h-screen bg-background">
