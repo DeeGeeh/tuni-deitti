170fbcc0eb8f81f5a5a98667ab6c791ca03eed42
Diar Ghaderi <diar.ghaderi@gmail.com>
2025-06-05 00:03:42 +0300

    better rerouting and loading

--- app/components/ProtectedRoute.tsx
+++ app/components/ProtectedRoute.tsx
@@ -1,6 +1,9 @@
+"use client";
+
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
 import { useAuth } from "../contexts/AuthContext";
+import LoadingSpinner from "./LoadingSpinner";
 
 interface ProtectedRouteProps {
   children: React.ReactNode;
@@ -12,12 +15,13 @@ const ProtectedRoute = ({ children }: ProtectedRouteProps) => {
 
   useEffect(() => {
     if (!loading && !user) {
-      router.push("/login");
+      const currentPath = window.location.pathname;
+      router.push(`/login?redirect=${encodeURIComponent(currentPath)}`);
     }
   }, [user, loading, router]);
 
   if (loading || !user) {
-    return <div>Loading...</div>;
+    return <LoadingSpinner fullScreen={true} size="lg" />;
   }
 
   return <>{children}</>;
