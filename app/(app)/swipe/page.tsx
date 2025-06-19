8c3f0620cfb1f1add9991991d62c620f5106a8fe
Diar Ghaderi <diar.ghaderi@gmail.com>
2025-06-19 19:48:39 +0300

    pass user Auth instead of user ID

--- app/(app)/swipe/page.tsx
+++ app/(app)/swipe/page.tsx
@@ -6,18 +6,22 @@ import SwipeableCard from "@/app/components/Profilecard";
 import { getUserProfile, getUsersToSwipe } from "@/app/lib/firebaseUtils";
 import { User } from "@/app/types/schema";
 import { useAuth } from "@/app/contexts/AuthContext";
+import { getAuth } from "firebase/auth";
+import { auth } from "@/app/lib/firebase";
 
 export default function SwipePage() {
   const { user } = useAuth();
   const [users, setUsers] = useState<User[]>([]);
   const [currentProfile, setCurrentProfile] = useState<User | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+  const auth = getAuth();
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         // Fetch both users to swipe and current user profile
         const [userData, currentUserProfile] = await Promise.all([
-          getUsersToSwipe(),
+          getUsersToSwipe(auth),
           user ? getUserProfile(user.uid) : Promise.resolve(null),
         ]);
 
@@ -25,6 +29,8 @@ export default function SwipePage() {
         setCurrentProfile(currentUserProfile);
       } catch (error) {
         console.error("Error fetching data:", error);
+      } finally {
+        setLoading(false);
       }
     };
 
